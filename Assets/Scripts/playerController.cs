6a582301b382f6e4bf7d098143b1ffe31d157bcd
Luis Filipe <luisf.modestop@gmail.com>
2018-11-20 19:04:12 -0200

    first commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+private Vector3 FirstTransform;
+private Vector3 DirectionTransform;
+private float DeltaX;
+private float DeltaY;
+public float speedX;
+public float speedY;
+public float MaxX;
+public float MaxY;
+public float MinX;
+public float MinY;
+private bool mouseButtonUp;
+public bool canMovePlayer;
+
+public Transform playerT;
+
+void Start(){
+    playerT = GetComponent<Transform>();
+}
+void Update()
+{
+ 		GetInformations();
+   		UpdatePlayerTransform();
+   
+}
+
+void GetInformations()
+{
+    if (Input.GetMouseButtonDown(0)) {
+        speedX = 1;
+        speedY = 1;
+        FirstTransform = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    }
+
+    if (Input.GetMouseButton(0)){
+        DirectionTransform = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    }
+
+    if (Input.GetMouseButtonUp(0)) {
+        mouseButtonUp = true;
+    }
+    if (mouseButtonUp) {
+        if (speedX > 0)
+            speedX -= 3 * Time.deltaTime;
+        else {
+            speedX = 0;
+            mouseButtonUp = false;
+			}
+		if (speedY > 0)
+            speedY -= 3 * Time.deltaTime;
+        else { 
+			speedY = 0;
+                mouseButtonUp = false;
+            }
+        }
+    }
+
+    void UpdatePlayerTransform()
+    {
+        DeltaX = (FirstTransform.x - DirectionTransform.x);
+        DeltaY = (FirstTransform.y - DirectionTransform.y);
+
+        playerT.position -= new Vector3(DeltaX * speedX, DeltaY * speedY, 0);
+
+        if (playerT.position.x < MinX)
+            playerT.position = new Vector3(MinX, playerT.position.y, playerT.position.z);
+
+        if (playerT.position.x > MaxX)
+            playerT.position = new Vector3(MaxX, playerT.position.y, playerT.position.z);
+
+        if (playerT.position.y > MaxY)
+            playerT.position = new Vector3(playerT.position.x, MaxY, playerT.position.z);
+
+        if (playerT.position.y < MinY)
+            playerT.position = new Vector3(playerT.position.x, MinY, playerT.position.z);
+
+    }
+}
